2e06bb39edca8de59c9f42fcf0fa4b642e2c15f3
ghubrakesh <sabalerakesh143@gmail.com>
2023-09-22 12:46:43 +0530

    added fastapi and uvicorn

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki as search_page
+from mylib.logic import search_wiki
 
-name = input("Enter a name: ")
+app = FastAPI()
 
-if name and name.isalpha:
-    print(wiki(name, 2))
-else:
-    print(wiki("wondder woman", 2))
+@app.get("/")
+async def home():
+    return {"message": "Enter the /search/<query> to search the pages or \n /wiki/<name> to get the summary of the page"}
+
+@app.get("/search/{query}")
+async def search(query:str):
+    res =  search_wiki(query)
+    d = dict()
+    for i in range(len(res)):
+        d[i+1] = res[i]
+    return {"result": d}
+
+@app.get("/wiki/{name}")
+async def wiki(name:str):
+    res = search_page(name)
+    return {"result": res}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,14 +1,11 @@
 import wikipedia
 
+def search_wiki(query):
+    """get search query results (page ids)"""
+    my_wiki = wikipedia.search(query)
+    return my_wiki
 
 def wiki(name="wondder woman", length=2):
     """This is wiki fetcher"""
-
     my_wiki = wikipedia.summary(name, sentences=length)
-    return my_wiki
-
-def search_wiki(query):
-    """get search query results (page ids)"""
-
-    my_wiki = wikipedia.search(query)
     return my_wiki
